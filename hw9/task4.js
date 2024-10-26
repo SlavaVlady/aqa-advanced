be935bfa82aee161f141b02499f9c71ff2254a0a
Macbook air <macbookair@MacBookAir.fritz.box>
2024-10-26 16:59:29 +0200

    HW9

---
+++ hw9/task4.js
@@ -0,0 +1,16 @@
+// Создаем объект person
+let person = {
+    firstName: "Александр",
+    lastName: "Коваль",
+    age: 30
+  };
+  
+  // Добавляем свойство email
+  person.email = "alex.koval@test.com";
+  
+  // Удаляем свойство age
+  delete person.age;
+  
+  // Выводим обновленный объект
+  console.log(person);
+  
